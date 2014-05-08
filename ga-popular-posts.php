62d492f82aa2dca4e94c0e76c5ec06089b3b5ca7
Maz <mazharul2007@gmail.com>
2014-05-08 16:22:11 +0800

    changes, first addition of files

---
+++ ga-popular-posts.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: GA popular posts
+ * Plugin URI: http://mazharulanwar.com/
+ * Description: Get the popular posts of your blog according to GA
+ * Version:  0.1
+ * Author: Mazharul Anwar
+ * Author URI: http://mazharulanwar.com/
+ * License: GPL3
+ */
+
